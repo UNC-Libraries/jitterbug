c7bd42b6c157a39ebc2d1a0e231a8d14c5d7408e
Andrew Shirk <ashirk@email.unc.edu>
2016-09-19 09:18:25 -0400

    Change order of related cuts and transfers

--- resources/views/masters/_related.blade.php
+++ resources/views/masters/_related.blade.php
@@ -1,39 +1,3 @@
-  @if (count($cuts) > 0 )
-  <div class="row">
-    <div class="col-xs-12 col-e0">
-      <h6>Related Cuts</h6>
-    </div>
-  </div>
-  <div class="row first last">
-    <div class="col-xs-12 col-fff">
-      <table id="related-cuts" class="table table-sm table-hover">
-        <thead>
-          <tr>
-            <th width="10%">PM #</th>
-            <th width="15%">Cut Number</th>
-            <th width="10%">Side</th>
-            <th width="20%">Title</th>
-            <th width="25%">Performer Composer</th>
-            <th width="20%">PM Start Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($cuts as $cut)
-          <tr role="button" data-master="{{$cut->preservationMasterId}}" data-id="{{$cut->id}}">
-            <td>{{$cut->preservationMasterId}}</td>
-            <td>{{$cut->cutNumber}}</td>
-            <td>{{$cut->side}}</td>
-            <td>{{$cut->title}}</td>
-            <td>{{$cut->performerComposer}}</td>
-            <td>{{$cut->pmStartTime}}</td>
-          </tr>
-          @endforeach                       
-        </tbody>
-      </table>
-    </div>
-  </div>
-  @endif
-
   @if (count($transfers) > 0 )
   <div class="row">
     <div class="col-xs-12 col-e0">
@@ -62,6 +26,42 @@
             <td>{{$transfer->engineerName}}</td>
             <td>{{--$transfer->vendor->name--}}</td>
             <td>{{$transfer->conditionNote}}</td>
+          </tr>
+          @endforeach                       
+        </tbody>
+      </table>
+    </div>
+  </div>
+  @endif
+  
+  @if (count($cuts) > 0 )
+  <div class="row">
+    <div class="col-xs-12 col-e0">
+      <h6>Related Cuts</h6>
+    </div>
+  </div>
+  <div class="row first last">
+    <div class="col-xs-12 col-fff">
+      <table id="related-cuts" class="table table-sm table-hover">
+        <thead>
+          <tr>
+            <th width="10%">PM #</th>
+            <th width="15%">Cut Number</th>
+            <th width="10%">Side</th>
+            <th width="20%">Title</th>
+            <th width="25%">Performer Composer</th>
+            <th width="20%">PM Start Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($cuts as $cut)
+          <tr role="button" data-master="{{$cut->preservationMasterId}}" data-id="{{$cut->id}}">
+            <td>{{$cut->preservationMasterId}}</td>
+            <td>{{$cut->cutNumber}}</td>
+            <td>{{$cut->side}}</td>
+            <td>{{$cut->title}}</td>
+            <td>{{$cut->performerComposer}}</td>
+            <td>{{$cut->pmStartTime}}</td>
           </tr>
           @endforeach                       
         </tbody>
