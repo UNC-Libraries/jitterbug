3348c416227b84243bd7393734404246820e8af7
Andrew Shirk <ashirk@email.unc.edu>
2017-01-07 08:46:50 -0500

    APPDEV-6612 Add timestamps to revision history

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -5,10 +5,23 @@ function routeName()
   return app('request')->route()->getName();
 }
 
+/**
+ * Used for imports and exports to produce a timestamp string
+ * for the uploaded or downloaded file. 
+ */
 function fileTimestamp()
 {
   $date = new DateTime();
   $date->setTimezone(new DateTimeZone('America/New_York'));
   $timestamp = $date->format('Y-m-d-H-i-sO');
   return $timestamp;
+}
+
+/**
+ * Used for displaying revision timestamps. 
+ */
+function revisionTimestamp($timestamp)
+{
+  $timestamp->setTimezone(new DateTimeZone('America/New_York'));
+  return $timestamp->format('m/d/Y h:i a');
 }
