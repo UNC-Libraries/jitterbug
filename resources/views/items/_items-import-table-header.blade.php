be6b900616765de14b2821e4fc1a56d93c3e03f7
sharonl <6546457+sharonluong@users.noreply.github.com>
2019-03-18 09:40:37 -0400

    APPDEV-7507 adding CSV upload ability for ReelTapeNumber (#14)

--- resources/views/items/_items-import-table-header.blade.php
+++ resources/views/items/_items-import-table-header.blade.php
@@ -4,11 +4,11 @@
         <th width="3%">Type</th>
         <th width="9%">Title</th>
         <th width="5%">Collection</th>
-        <th width="10%">ContainerNote</th>
+        <th width="9%">ContainerNote</th>
         <th width="7%">AccessionNumber</th>
         <th width="4%">LegacyID</th>
-        <th width="5%">FormatID</th>
-        <th width="7%">RecLocation</th>
+        <th width="4%">FormatID</th>
+        <th width="6%">RecLocation</th>
         <th width="4%">ItemYear</th>       
         <th width="5%">ItemDate</th>
         <th width="4%">Size</th>
@@ -17,6 +17,7 @@
         <th width="4%">Color</th>
         <th width="5%">SoundType</th>
         <th width="6%">LengthInFeet</th>
-        <th width="11%">ContentDescription</th>
+        <th width="9%">ContentDescription</th>
+        <th width="5%">ReelTapeNumber</th>
       </tr>
     </thead>
