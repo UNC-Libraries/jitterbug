8f06c05346c4f6bb2893fddbb20f722da3eb862b
Sharon Luong <snluong@email.lib.unc.edu>
2019-03-19 16:54:18 -0400

    APPDEV-8583 adding clarification to call number sequence test

--- tests/Unit/CallNumberSequenceTest.php
+++ tests/Unit/CallNumberSequenceTest.php
@@ -12,20 +12,20 @@ class CallNumberSequenceTest extends TestCase
    * @return void
    */
 
-  public function testNextAlwaysUseNewCallSequence()
+  public function testNextAlwaysUsesNewCallSequenceIfPrefixInNewStyleArray()
   {
     $new_prefix_array = Models\CallNumberSequence::ALWAYS_USE_NEW_STYLE;
     $collectionId = 20005;
     $format = factory(Models\Format::class)->create([
-        'prefix' => $new_prefix_array[array_rand(@$new_prefix_array)],
+        'prefix' => $new_prefix_array[array_rand($new_prefix_array)],
     ]);
 
     $sequence = Models\CallNumberSequence::next($collectionId, $format->id);
     $this->assertTrue(is_a($sequence,'Jitterbug\Models\NewCallNumberSequence'),
-      'Call Number is not a NewCallNumberSequence, as it should be.');
+      'Sequence is not a NewCallNumberSequence, as it should be.');
   }
 
-  public function testNextReturnsPrecedingNewCallNumberSequence()
+  public function testNextReturnsNewCallNumberSequenceIfItAlreadyExists()
   {
     $collectionId = 20006;
     $callNumber = factory(Models\NewCallNumberSequence::class)->create([
@@ -39,10 +39,12 @@ class CallNumberSequenceTest extends TestCase
 
     $sequence = Models\CallNumberSequence::next($collectionId, $format->id);
     $this->assertSame($callNumber->id, $sequence->id,
-      'Call Number is not the existing last NewCallNumberSequence, as it should be.');
+      'Sequence is not the existing NewCallNumberSequence, as it should be.');
+    $this->assertTrue(is_a($sequence,'Jitterbug\Models\NewCallNumberSequence'),
+      'Sequence is not a NewCallNumberSequence, as it should be.');
   }
 
-  public function testNextReturnsPrecedingLegacyCallNumberSequence()
+  public function testNextReturnsExistingLegacyCallNumberSequenceIfNoNewCallNumberSequenceExists()
   {
     $collectionId = 20007;
     $prefix = 'CD';
@@ -51,7 +53,7 @@ class CallNumberSequenceTest extends TestCase
       'collectionId' => 20013,
       'next' => 2
     ]);
-    factory(Models\LegacyCallNumberSequence::class)->create([
+    $legacyCallNumber = factory(Models\LegacyCallNumberSequence::class)->create([
       'prefix' => $prefix,
       'next' => 2
     ]);
@@ -61,6 +63,8 @@ class CallNumberSequenceTest extends TestCase
 
     $sequence = Models\CallNumberSequence::next($collectionId, $format->id);
     $this->assertTrue(is_a($sequence,'Jitterbug\Models\LegacyCallNumberSequence'),
-      'Call Number is not a LegacyCallNumberSequence, as it should be.');
+      'Sequence is not a LegacyCallNumberSequence, as it should be.');
+    $this->assertSame($legacyCallNumber->id, $sequence->id,
+      'Sequence is not the existing legacyCallNumberSequence, as it should be.');
   }
 }
