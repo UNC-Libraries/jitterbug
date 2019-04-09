54e637f3cb49f8e57d1000862b79a9399f39f1d1
Sharon Luong <snluong@email.lib.unc.edu>
2019-04-09 16:17:58 -0400

    APPDEV-7959 printing out response to debug on circleci

--- tests/Feature/TransferAudioImportsTest.php
+++ tests/Feature/TransferAudioImportsTest.php
@@ -73,7 +73,7 @@ class TransferAudioImportsTest extends TestCase
                        ->post('/transfers/batch/audio-import-execute',
                          [],
                          array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
-
+      print($response->getContent());
       $responseArray = json_decode($response->getContent(), true);
       $htmlContainsSuccessMessage = strpos($responseArray['html'], 'Your import was successful!') !== false;
 
