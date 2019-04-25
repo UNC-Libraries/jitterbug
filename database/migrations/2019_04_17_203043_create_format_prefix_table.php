48d7713c7943c9bcc11caef1a861226184c78b7f
sharonl <6546457+sharonluong@users.noreply.github.com>
2019-04-25 16:13:22 -0400

    APPDEV-8637 initial migrations for non sfc prefixes (#21)

---
+++ database/migrations/2019_04_17_203043_create_format_prefix_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormatPrefixTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('format_prefix', function (Blueprint $table) {
+        $table->smallInteger('format_id')->unsigned();
+        $table->foreign('format_id')->references('id')->on('formats')->onDelete('cascade');
+
+        $table->integer('prefix_id')->unsigned();
+        $table->foreign('prefix_id')->references('id')->on('prefixes')->onDelete('cascade');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('format_prefix');
+    }
+}
