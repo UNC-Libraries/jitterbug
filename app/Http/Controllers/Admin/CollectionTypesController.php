7f98adca748534b89de3951aa285e279f91a3afa
Sharon Luong <snluong@email.lib.unc.edu>
2019-04-29 15:58:06 -0400

    APPDEV-8638 setting up collection type and prefix requests, filling out controllers

--- app/Http/Controllers/Admin/CollectionTypesController.php
+++ app/Http/Controllers/Admin/CollectionTypesController.php
@@ -2,9 +2,12 @@
 
 use Auth;
 use DB;
+use Jitterbug\Http\Requests\CollectionTypeRequest;
 use Log;
 use Jitterbug\Http\Controllers\Controller;
 use Jitterbug\Models\CollectionType;
+use Jitterbug\Models\Collection;
+use Jitterbug\Models\Prefix;
 
 
 use Illuminate\Http\Request;
@@ -23,5 +26,58 @@ class CollectionTypesController extends Controller
   public function __construct()
   {
     $this->middleware(['auth', 'admin']);
+  }
+
+  public function index(Request $request) {
+    if ($request->ajax()) {
+      $records = CollectionType::orderBy('name')->get();
+      return view('admin._names', compact('records'));
+    }
+  }
+
+  public function store(CollectionTypeRequest $request) {
+    if ($request->ajax()) {
+      $input = $request->all();
+      $collectionType = new CollectionType;
+      $collectionType->fill($input);
+      $collectionType->save();
+      return response()->json($collectionType);
+    }
+  }
+
+  public function update($id, CollectionTypeRequest $request) {
+    if ($request->ajax()) {
+      $input = $request->all();
+
+      $collectionType = CollectionType::findOrFail($id);
+      $collectionType->fill($input);
+
+      if ($collectionType->isDirty()) {
+
+        // Update MySQL
+        DB::transaction(function () use ($collectionType) {
+          $collectionType->save();
+        });
+      }
+    }
+  }
+
+  public function destroy($id, Request $request) {
+    if ($request->ajax()) {
+      $collectionCount = Collection::where('collection_type_id', $id)->count();
+      $prefixCount = Prefix::where('collection_type_id', $id)->count();
+      if ($collectionCount === 0 && $prefixCount === 0) {
+        $collectionType = Collection::findOrFail($id);
+        $collectionType->delete();
+        $response = array('status'=>'success');
+        return response()->json($response);
+      } else {
+        $bag = new MessageBag();
+        $bag->add('status', 'Looks like that collection type is currently ' .
+          'in use. Change the collection type of the related collections ' .
+          'and/or prefixes before deleting.');
+        return response()->json($bag, 422);
+      }
+    }
   }
 }
--- app/Http/Controllers/Admin/PrefixesController.php
+++ app/Http/Controllers/Admin/PrefixesController.php
@@ -24,4 +24,57 @@ class PrefixesController extends Controller
   {
     $this->middleware(['auth', 'admin']);
   }
+
+  public function index(Request $request) {
+    if ($request->ajax()) {
+      $records = Prefix::orderBy('name')->get();
+      return view('admin._names', compact('records'));
+    }
+  }
+
+  public function store(PrefixRequest $request) {
+    if ($request->ajax()) {
+      $input = $request->all();
+      $prefix = new Prefix;
+      $prefix->fill($input);
+      $prefix->save();
+      return response()->json($prefix);
+    }
+  }
+
+  public function update($id, PrefixRequest $request) {
+    if ($request->ajax()) {
+      $input = $request->all();
+
+      $prefix = Prefix::findOrFail($id);
+      $prefix->fill($input);
+
+      if ($prefix->isDirty()) {
+
+        // Update MySQL
+        DB::transaction(function () use ($prefix) {
+          $prefix->save();
+        });
+      }
+    }
+  }
+
+  public function destroy($id, Request $request) {
+    if ($request->ajax()) {
+      $prefix = Prefix::findOrFail($id);
+      $formatCount = $prefix->formats->count();
+      if ($collectionCount === 0 && $prefixCount === 0) {
+        $bag = new MessageBag();
+        $prefix->delete();
+        $response = array('status'=>'success');
+        return response()->json($response);
+      } else {
+        $bag = new MessageBag();
+        $bag->add('status', 'Looks like that collection type is currently ' .
+          'in use. Change the collection type of the related collections ' .
+          'and/or prefixes before deleting.');
+        return response()->json($bag, 422);
+      }
+    }
+  }
 }
