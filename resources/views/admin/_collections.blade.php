412d702f6ba22195ccc39b98673f20220bbc7bb8
Sharon Luong <snluong@email.lib.unc.edu>
2019-06-07 16:03:33 -0400

    APPDEC-8640 hiding in progress in views

--- resources/views/admin/_collections.blade.php
+++ resources/views/admin/_collections.blade.php
@@ -35,7 +35,7 @@
       <form class="form-inline">
         <input type="text" name="id" class="form-control form-control-sm" maxlength="8" placeholder="Id" autocomplete="off" style="width: 65px;">
         <input type="text" name="name" class="form-control form-control-sm" maxlength="255" placeholder="Name" autocomplete="off" style="width: 250px;">
-        {!! Form::select('collectionTypeId', $collectionTypes, null, array('class' => 'form-control form-control-sm')) !!}
+{{--   TODO APPDEV-8640     {!! Form::select('collectionTypeId', $collectionTypes, null, array('class' => 'form-control form-control-sm')) !!}--}}
         <button class="btn btn-sm btn-secondary popover-submit" type="submit"><i class="fa fa-fw fa-check" aria-hidden="true"></i></button>
         <button class="btn btn-sm btn-secondary cancel-new-record"><i class="fa fa-fw fa-ban" aria-hidden="true"></i></button>
       </form>
