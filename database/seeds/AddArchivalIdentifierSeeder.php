318cfabe3a8b61d52b24d8afc3177367569c55b9
Sharon Luong <snluong@email.lib.unc.edu>
2019-06-10 16:43:46 -0400

    APPDEV-8722 added migration file and seeder file for new column in collections

---
+++ database/seeds/AddArchivalIdentifierSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Jitterbug\Models\Collection;
+
+class AddArchivalIdentifierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // to backfill the collection IDs as strings in the archival_identifier column
+      $collections = Collection::all();
+
+      foreach ($collections as $collection) {
+        $collection->archivalIdentifier = (string) $collection->id;
+        $collection->save();
+      }
+    }
+}
