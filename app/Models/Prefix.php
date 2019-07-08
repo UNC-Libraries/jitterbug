767376c49a09dbfd107a26218046fd41956a67f5
Sharon Luong <snluong@email.lib.unc.edu>
2019-07-08 12:04:22 -0400

    APPDEV-8796 added new static method and changed call to new method for determining prefixes

--- app/Models/Prefix.php
+++ app/Models/Prefix.php
@@ -5,6 +5,7 @@ namespace Jitterbug\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Prefix extends Model
 {
@@ -26,5 +27,30 @@ class Prefix extends Model
   public function detachAllFormats()
   {
     $this->formats()->detach();
+  }
+
+  public static function findPrefixLabel($formatId, $collectionId)
+  {
+    $collectionTypeId = DB::table('collections')->select('collection_type_id')
+                                                ->where('id', '=', $collectionId)
+                                                ->get()
+                                                ->first()->collection_type_id;
+
+    # find the prefix attached to the specified format
+    # that has the same collection type ID as the specified collection
+    $label = DB::table('prefixes')->select('label')
+                                  ->join('format_prefix', 'prefixes.id', '=', 'format_prefix.prefix_id')
+                                  ->where([
+                                    ['format_prefix.format_id', '=', $formatId],
+                                    ['prefixes.collection_type_id', '=', $collectionTypeId]
+                                  ])
+                                  ->get()
+                                  ->first()->label;
+
+    if ($label === null) {
+      $label = Format::findOrFail($formatId)->prefix;
+    }
+
+    return $label;
   }
 }
