1ead35def6b84714472cce0c825e832212d098c6
Sharon Luong <snluong@email.lib.unc.edu>
2019-07-17 14:13:12 -0400

    APPDEV-8796 removing test clutter

--- tests/Unit/PrefixTest.php
+++ tests/Unit/PrefixTest.php
@@ -11,16 +11,14 @@ class PrefixTest extends TestCase
   use DatabaseTransactions;
   private $prefix1;
   private $prefix2;
-  private $format1;
-  private $format2;
+  private $format;
 
   protected function setUp()
   {
     parent::setUp();
     $this->prefix1 = factory(Prefix::class)->create(['deleted_at' => null, 'collection_type_id' => 3]);
     $this->prefix2 = factory(Prefix::class)->create(['deleted_at' => null, 'collection_type_id' => 4]);
-    $this->format1 = factory(Format::class)->create();
-    $this->format2 = factory(Format::class)->create();
+    $this->format = factory(Format::class)->create();
   }
 
   public function testFindPrefixLabelFindsLabelSuccessfully()
@@ -28,15 +26,14 @@ class PrefixTest extends TestCase
     $collection = factory(Collection::class)->create(['collection_type_id' => 3]);
 
     // attach the formats to the prefix
-    $this->format1->prefixes()->attach($this->prefix1->id);
-    $this->format2->prefixes()->attach([$this->prefix1->id, $this->prefix2->id]);
+    $this->format->prefixes()->attach([$this->prefix1->id, $this->prefix2->id]);
 
-    $this->assertEquals($this->prefix1->label, Prefix::findPrefixLabel($this->format2->id, $collection->id));
+    $this->assertEquals($this->prefix1->label, Prefix::findPrefixLabel($this->format->id, $collection->id));
   }
 
   public function testFindPrefixLabelRaisesExceptionForNullCollectionTypeId()
   {
     $this->expectException(NotFoundHttpException::class);
-    Prefix::findPrefixLabel($this->format1->id, 20001);
+    Prefix::findPrefixLabel($this->format->id, 20001);
   }
 }
