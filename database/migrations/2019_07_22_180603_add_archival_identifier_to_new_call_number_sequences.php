cfefaed03b39b437b802a73af663ac4c1c96026e
Sharon Luong <snluong@email.lib.unc.edu>
2019-07-22 14:55:21 -0400

    APPDEV-8733 adding migration file for archival identifier on new call number sequences

---
+++ database/migrations/2019_07_22_180603_add_archival_identifier_to_new_call_number_sequences.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddArchivalIdentifierToNewCallNumberSequences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('new_call_number_sequences', function (Blueprint $table) {
+        $table->string('archival_identifier')->after('collection_id')->unique()->nullable();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('new_call_number_sequences', function (Blueprint $table) {
+        $table->dropColumn('archival_identifier');
+      });
+    }
+}
