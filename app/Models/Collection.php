5ba20f33ae6e0c0b47bf128724330275bba06476
Sharon Luong <snluong@email.lib.unc.edu>
2019-09-20 16:40:48 -0400

    APPDEV-8779 turning methods into artisan command

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class Collection extends Model {
   use CamelCasing;
@@ -32,47 +30,5 @@ class Collection extends Model {
   public function identifiableName()
   {
     return $this->name;
-  }
-
-  public static function autoIncrementIdsScript()
-  {
-    $collections = self::withTrashed()->get();
-    $idTracker = 1;
-    $collectionIdMapping = [];
-
-    foreach ($collections as $collection) {
-      $oldId = $collection->id;
-      $collection->id = $idTracker;
-      $collection->save();
-
-      $collectionIdMapping[$oldId] = $idTracker;
-
-      $idTracker++;
-    }
-    return $collectionIdMapping;
-  }
-
-  // $tableName should be snake case, plural string
-  public static function updateCollectionIdInTable($tableName, $collectionIdMapping)
-  {
-    $brokenIds = array();
-    DB::table($tableName)
-      ->whereNotNull('collection_id')
-      ->chunkById(1000, function ($results) use (&$brokenIds, &$collectionIdMapping, $tableName) {
-       foreach ($results as $result) {
-         if (isset($collectionIdMapping[$result->collection_id])) {
-           $newCollectionId = $collectionIdMapping[$result->collection_id];
-         } else {
-           $brokenIds[] = $result->id;
-           continue;
-         }
-
-         DB::table($tableName)
-           ->where('id', $result->id)
-           ->update(['collection_id' => $newCollectionId]);
-       }
-    });
-
-    return $brokenIds;
   }
 }
