a7b58988e6c1a9c65b10cca3d8b5614dece8386c
Sharon Luong <snluong@email.lib.unc.edu>
2020-02-20 11:03:05 -0500

    APPDEV-9007 changing env variable name back because that's not the problem

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_NEW_HOST', 'localhost'),
+			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
