f2252258a68b64e56a68c3cc6cdda30eeaf1a144
Sharon Luong <snluong@email.lib.unc.edu>
2020-02-26 13:56:44 -0500

    APPDEV-9007 reducing the wait time to remove the mutex lock in the cron job scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel {
       ->everyMinute()
       ->timezone('America/New_York')
       ->between('7:00', '18:00')
-      ->withoutOverlapping(10);
+      ->withoutOverlapping(2);
   }
 
 }
