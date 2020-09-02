159444f77622662a875ca46248212b05da53a9c0
Sharon Luong <snluong@email.lib.unc.edu>
2020-09-02 17:07:23 -0400

    APPDEV-9701 added more tests for user inactivation and reactivation

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Jitterbug\Models\User;
+use Jitterbug\Models\Mark;
 
 class UsersTest extends TestCase
 {
@@ -24,6 +25,40 @@ class UsersTest extends TestCase
     $user = $user->fresh();
 
     $this->assertSame(1, $user->inactive, "User's inactive status should be 1");
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
+  public function testInactivateDeletesUserMarks() : void
+  {
+    $user = factory(User::class)->create(['inactive' => 0]);
+    $adminUser = factory(User::class)->create(['admin' => 1]);
+    factory(Mark::class)->create(['user_id' => $user->id]);
+    factory(Mark::class)->create(['user_id' => $user->id]);
+    $this->assertCount(2, $user->marks);
+
+    $this->be($adminUser);
+    $response = $this->post('/users/inactivate',
+      ['id' => $user->id],
+      array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+    $user = $user->fresh();
+
+    $this->assertCount(0, $user->marks, "User's marks should have been deleted.");
+    $response->assertJson(['marksDeleted' => 2]);
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
+  public function testReactivateSetsInactiveToFalse() : void
+  {
+    $user = factory(User::class)->create(['inactive' => 1]);
+    $adminUser = factory(User::class)->create(['admin' => 1]);
+
+    $this->be($adminUser);
+    $response = $this->post('/users/reactivate',
+      ['id' => $user->id],
+      array('HTTP_X-Requested-With' => 'XMLHttpRequest'));
+    $user = $user->fresh();
+
+    $this->assertSame(0, $user->inactive, "User's inactive status should be 0");
     $this->assertEquals(200, $response->getStatusCode());
   }
 }
