1c3b4aacc3435c18d0cdb989a296bb1bb76b7dde
Sharon Luong <snluong@email.lib.unc.edu>
2021-07-19 17:45:38 -0400

    APPDEV-10093 adding edit cut integration test

--- tests/Browser/NewCutTest.php
+++ tests/Browser/NewCutTest.php
@@ -3,10 +3,10 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Log;
 use Jitterbug\Models\AudioTransfer;
 use Jitterbug\Models\Transfer;
 use Jitterbug\Models\User;
+use Jitterbug\Models\Cut;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Venturecraft\Revisionable\Revision;
@@ -15,33 +15,64 @@ class NewCutTest extends DuskTestCase
 {
   use DatabaseMigrations;
 
-  public function testUserIsAbleToAddCutWhenNecessary() : void
+  private $user;
+  private $transfer;
+  protected function setUp(): void
   {
-    $user = User::factory()->create();
+    parent::setUp();
+    $this->user = User::factory()->create();
     $audioTransfer = AudioTransfer::factory()->create();
-    $transfer = Transfer::factory()->create([
-      'engineer_id' => $user->id,
+    $this->transfer = Transfer::factory()->create([
+      'engineer_id' => $this->user->id,
       'subclass_type' => 'AudioTransfer',
       'subclass_id' => $audioTransfer->id]);
-    // add user_id to automatically generated related revision
-    $revision = Revision::where('revisionable_type', 'Transfer')
-      ->where('revisionable_id', $transfer->id)
-      ->get()
-      ->first();
-    $revision->user_id = $user->id;
-    $revision->save();
+    $this->generateRevision('Transfer', $this->transfer->id);
+  }
 
+  public function testUserIsAbleToAddCutWhenNecessary() : void
+  {
     $this->browse(function (Browser $browser) {
-      $browser->loginAs(1)
-        ->visit('/transfers/1')
+      $path = '/transfers/' . $this->transfer->id;
+      $browser->loginAs($this->user->id)
+        ->visit($path)
         ->assertSee('Add Cut')
 
         ->clickLink('Add Cut')
         ->assertPathBeginsWith('/cuts/create')
         ->type('side', '1')
         ->press('Save')
-        ->assertPathIs('/transfers/1')
+        ->assertPathIs($path)
         ->assertSee('Related Cut');
     });
+  }
+
+  public function testUserMayEditCut() : void
+  {
+    $this->browse(function (Browser $browser) {
+      $cut = Cut::factory()->create();
+      $this->generateRevision('Cut', $cut->id);
+      
+      $browser->loginAs($this->user->id)
+        ->visit('/cuts/' . $cut->id . '?instanceId=' . $cut->preservation_instance_id)
+        ->clickLink('Edit')
+        ->assertPathIs('/cuts/' . $cut->id . '/edit')
+
+        ->type('title', 'The best film ever')
+        ->press('Save')
+        ->assertPathIs('/cuts/' . $cut->id);
+    });
+  }
+
+  // when a factory object is generated, if that model uses Revisionable
+  // a revision is automatically created without a user_id
+  // this adds a necessary user_id to to that revision
+  public function generateRevision($revisionableType, $revisionableId) : void
+  {
+    $revision = Revision::where('revisionable_type', $revisionableType)
+      ->where('revisionable_id', $revisionableId)
+      ->get()
+      ->first();
+    $revision->user_id = $this->user->id;
+    $revision->save();
   }
 }
