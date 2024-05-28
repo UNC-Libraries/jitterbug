80bfc5cbbf9f14a7ce7423def60c15a8199512b9
lfarrell <lfarrell@email.unc.edu>
2024-05-28 15:36:21 -0400

    Upgrade to laravel 10

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,10 +14,10 @@ const mix = require('laravel-mix');
 mix.sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.scripts([
-  'node_modules/js-cookie/src/js.cookie.js',
+  'node_modules/js-cookie/dist/js.cookie.min.js',
   'node_modules/bootstrap/dist/js/bootstrap.min.js',
   // We're using a very slightly modified version of colResizable,
   // so have made a copy of the source to the public directory.
   'public/js/colResizable-1.6.js',
-  'node_modules/devbridge-autocomplete/dist/jquery.autocomplete.js'
+  'node_modules/devbridge-autocomplete/dist/jquery.autocomplete.min.js'
 ], 'public/js/vendor.js');
