a73d7f63ac86abe5719cdfb84b9d0930bf0724ef
Shift <shift@laravelshift.com>
2024-05-29 14:24:27 +0000

    Convert `Html` facade

--- resources/views/shared/_footer.blade.php
+++ resources/views/shared/_footer.blade.php
@@ -1,6 +1,6 @@
         <div class="row">
 	      <div class="col-md-12">
-	        <div style="float: left; margin-top: 1rem;">{!! Html::image('img/unc-logo.png', 'UNC logo', array('width' => 153 , 'height' => 34)) !!}</div>
-	        <div style="float: right; margin-top: 1rem;">{!! Html::image('img/mellon-logo.png', 'Andrew W. Mellon Foundation logo', array('width' => 78 , 'height' => 36)) !!}</div>
+	        <div style="float: left; margin-top: 1rem;">{{ html()->img(asset('img/unc-logo.png'), 'UNC logo')->attribute('width', 153)->attribute('height', 34) }}</div>
+	        <div style="float: right; margin-top: 1rem;">{{ html()->img(asset('img/mellon-logo.png'), 'Andrew W. Mellon Foundation logo')->attribute('width', 78)->attribute('height', 36) }}</div>
 	      </div>
         </div>
