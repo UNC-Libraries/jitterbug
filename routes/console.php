2dc20f87bc82d5485281b9a34c0b4bc83974c381
Shift <shift@laravelshift.com>
2025-07-17 14:25:42 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Jitterbug\Presenters\ActivityStream;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// Generate the activity stream
+Schedule::call(function () {
+    $activityStream = new ActivityStream;
+    $activityStream->generate();
+})->name('generateActivityStream')
+    ->everyMinute()
+    ->timezone('America/New_York')
+    ->between('7:00', '18:00')
+    ->withoutOverlapping(15);
