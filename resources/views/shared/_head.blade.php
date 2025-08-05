311e662822120b9e6e80d82e31ae98370b006ce9
lfarrell <lfarrell@email.unc.edu>
2025-08-05 15:13:37 -0400

    Move font-awesome to an import

--- resources/views/shared/_head.blade.php
+++ resources/views/shared/_head.blade.php
@@ -3,10 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-
   <link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:300' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,400italic,600italic' rel='stylesheet' type='text/css'>
-  <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type='text/css'>
+
   <link href="{{ asset('css/jquery.dataTables.min.css') }}"  rel="stylesheet" type="text/css"/>
   <link href="{{ asset('css/flatpickr.min.css') }}" rel="stylesheet">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
